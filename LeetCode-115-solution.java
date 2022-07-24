3ca0f7312979d40e54ffc540f782527a647851bd
Hcodemaster <2274229970@qq.com>
2022-07-24 17:38:10 +0800

    update question 115 solution

--- LeetCode-115-solution.java
+++ LeetCode-115-solution.java
@@ -1,3 +1,8 @@
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Set;
+
 class Solution {
     class  LinkNode
     {
@@ -74,6 +79,47 @@ class Solution {
             }
             if(sum == N)
                 break;
+        }
+        return true;
+    }
+    /*第二种拓扑排序的解法
+     * 存储节点之间的关系时不用链表
+     * 直接使用Set集合存储
+     * 通过队列保存每一个入度为0的节点
+     */
+    public boolean sequenceReconstruction2(int[] nums, int[][] sequences) {
+        int N = nums.length;
+        Set<Integer>[] garph = new Set[N+1];
+        for(int i=1;i<=N;i++)
+        {
+            garph[i] = new HashSet<>();
+        }
+        int[] in_degree = new int[N+1];
+        for(int[] sequence : sequences)//根据sequences构造图并计算所有节点的入度
+        {
+            for(int i=0;i<sequence.length-1;i++)
+            {
+                garph[sequence[i]].add(sequence[i+1]);
+                in_degree[sequence[i+1]] ++ ;
+            }
+        }
+        Queue<Integer> queue = new LinkedList<>();
+        for(int i=1;i<=N;i++)
+        {
+            if(in_degree[i] == 0)
+                queue.add(i);
+        }
+        while(queue.size()>0)
+        {
+            if(queue.size()>1)
+                return false;
+            int candicate = queue.poll();//得到拓扑排序的节点
+            for(Integer val : garph[candicate])//更新剩余节点的入度值
+            {
+                in_degree[val] --;
+                if(in_degree[val] == 0)
+                    queue.offer(val);
+            }
         }
         return true;
     }
